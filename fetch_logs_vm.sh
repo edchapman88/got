a86a41d1e45f679139c25502c58bdda584f6e682
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-03-17 15:13:59 +0000

    experiment running blue and shrew on virtualised infra

--- fetch_logs_vm.sh
+++ fetch_logs_vm.sh
@@ -2,6 +2,8 @@ date=$(date '+%Y-%m-%d_%H:%M:%S')
 
 scp blue@$HILBERT_VM:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/hilbert/$date.json
 
+scp blue@$HILBERT_VM:/home/blue/blue_logs/* ~/projects/got/experiments/$1/logs/hilbert
+
 scp blue@$KLEENE_VM:/var/log/telegraf/telegraf.json ~/projects/got/experiments/$1/logs/kleene/$date.json
 
 scp blue@$KLEENE_VM:/home/blue/got_logs/* ~/projects/got/experiments/$1/logs/kleene
